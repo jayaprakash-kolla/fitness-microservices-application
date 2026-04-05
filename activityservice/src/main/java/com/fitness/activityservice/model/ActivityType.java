205c263fc183bf2af59be60be63261282757a444
Jaya Prakash <k.jayaprakash1923@gmail.com>
2026-04-06 02:33:46 +0530

    Initial implementation of ActivityService

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,11 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    YOGA,
+    HIIT,
+    WEIGHT_TRAINING
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,55 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getActivityType())
+                .duration(request.getDuration())
+                .caloriesBurnt(request.getCaloriesBurnt())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse activityResponse = new ActivityResponse();
+
+        activityResponse.setId(activity.getId());
+        activityResponse.setUserId(activity.getUserId());
+        activityResponse.setType(activity.getType());
+        activityResponse.setDuration(activity.getDuration());
+        activityResponse.setCaloriesBurnt(activity.getCaloriesBurnt());
+        activityResponse.setStartTime(activity.getStartTime());
+        activityResponse.setAdditionalMetrics(activity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(activity.getCreatedAt());
+        activityResponse.setUpdatedAt(activity.getUpdatedAt());
+
+        return activityResponse;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        return activityRepository.findByUserId(userId)
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+}
